57a83abd4da77a0eefc600827556366f45d0cbc9
Aditya Singh <131813027+adityaSingh6271@users.noreply.github.com>
2024-01-24 12:04:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function volume_sphere() {
-    // Get the radius value from the input field
+    //Write your code here
     var radius = parseFloat(document.getElementById('radius').value);
 
     // Check if the entered value is a valid number
@@ -17,10 +17,6 @@ function volume_sphere() {
     // Prevent the form from submitting
     return false;
 }
+ 
 
-window.onload = function () {
-    document.getElementById('MyForm').onsubmit = function () {
-        volume_sphere();
-        return false;
-    };
-};
+window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
